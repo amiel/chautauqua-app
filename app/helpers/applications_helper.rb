3c998ccce8c72f06dda8aa169a6423b6a7b211c9
Amiel Martin <amiel.martin@gmail.com>
2010-03-03 23:47:08 -0800

    better viewing of applications/show

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -6,6 +6,22 @@ module ApplicationsHelper
       abilities[application.other_abilities] = true
     end
     
-    truncate abilities.keys.collect(&:to_s).collect(&:humanize).to_sentence(:last_word_connector => ', ', :two_words_connector => ', ')
+    abilities.keys.collect(&:to_s).collect(&:humanize).to_sentence(:last_word_connector => ', ', :two_words_connector => ', ')
+  end
+  
+  def formtastic_label(name)
+    t(:"formtastic.labels.application.#{name}", :default => name.to_s.titleize)
+  end
+  
+  def basic_attribute(name, options = {})
+    attribute name, options do |name|
+      v = h(@application.send(name))
+      options[:simple_format] ? simple_format(v) : v
+    end
+  end
+  
+  def attribute(name, options = {})
+    content_tag(:dt, formtastic_label(name), options[:dt]) +
+    content_tag(:dd, yield(name), options[:dd])
   end
 end
