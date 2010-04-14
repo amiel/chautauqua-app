73b8d335e0977fea8a1cd23ab4cdcbd9e80f89c8
Amiel Martin <amiel.martin@gmail.com>
2010-04-14 00:10:11 -0700

    oops, the application is shown before the deadline

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :applications, :member => { :complete => :get }, :collection => { :only_new => :get }
   
   if ApplicationDeadline.before_deadline? then
-    map.root :controller => 'applications', :action => 'closed'
-  else
     map.root :controller => 'applications', :action => 'new'
+  else
+    map.root :controller => 'applications', :action => 'closed'
   end
 end
