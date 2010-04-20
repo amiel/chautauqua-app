39f2bfa136095a52f3a2f2ca99f8426c199f7a67
Amiel Martin <amiel.martin@gmail.com>
2010-04-20 11:21:55 -0700

    extend deadline

--- config/initializers/application_deadline.rb
+++ config/initializers/application_deadline.rb
@@ -1,4 +1,5 @@
-APPLICATION_DEADLINE = Time.zone.parse 'March 31st, 2010 23:59:59'
+# APPLICATION_DEADLINE = Time.zone.parse 'March 31st, 2010 23:59:59'
+APPLICATION_DEADLINE = Time.zone.parse 'April 31st, 2010 23:59:59'
 module ApplicationDeadline
   def self.before_deadline?
     Time.current < APPLICATION_DEADLINE
